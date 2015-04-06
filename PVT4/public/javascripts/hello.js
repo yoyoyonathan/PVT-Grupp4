bd1d72d97d40ac2904f535ba7ff8e16cc38e8312
Fortet9000 <adam.kvarfordt@gmail.com>
2015-04-06 13:26:36 +0200

    Update hello.js

--- PVT4/public/javascripts/hello.js
+++ PVT4/public/javascripts/hello.js
@@ -1,3 +1,3 @@
 if (window.console) {
-  console.log("Welcome to your Play application's JavaScript!");
+  console.log("Welcome to your Play application's JavaScript! Nu testar vi att ändra här.");
 }
