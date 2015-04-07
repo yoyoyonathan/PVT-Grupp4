c9f07a3bf63864281c706119de53544fec85719b
Fortet9000 <adam.kvarfordt@gmail.com>
2015-04-07 13:43:46 +0200

    Update Application.java

--- PVT4/app/controllers/Application.java
+++ PVT4/app/controllers/Application.java
@@ -10,10 +10,6 @@ public class Application extends Controller {
     public static Result index() {
         return ok(index.render("Testtesttest2222."));
     }
-    
-    public static Result artister() {
-        return ok(artister.render("Artister.scala.html"));
-    }
 }
 
 
