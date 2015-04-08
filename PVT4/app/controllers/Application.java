bfd2f05bd44eb9f72bd5dc39984065531f42550a
Fortet9000 <adam.kvarfordt@gmail.com>
2015-04-08 13:29:38 +0200

    saker

--- PVT4/app/controllers/Application.java
+++ PVT4/app/controllers/Application.java
@@ -2,13 +2,16 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
-
 import views.html.*;
 
 public class Application extends Controller {
-
+	
     public static Result index() {
-        return ok(index.render("Testtesttest2222."));
+        return ok(index.render());
+    }
+    
+    public static Result artister() {
+        return ok(artister.render());
     }
 }
 
--- PVT4/build.sbt
+++ PVT4/build.sbt
@@ -18,3 +18,6 @@ libraryDependencies ++= Seq(
   cache,
   javaWs
 )
+
+
+fork in run := true
