df20c17f04185427577db3db721b07d70b6c8ad2
Fortet9000 <adam.kvarfordt@gmail.com>
2015-04-21 11:12:20 +0200

    Submit form saker

--- PVT4/app/controllers/Application.java
+++ PVT4/app/controllers/Application.java
@@ -1,6 +1,8 @@
 package controllers;
 
+import models.User;
 import play.*;
+import play.data.Form;
 import play.mvc.*;
 import views.html.*;
 
@@ -16,6 +18,12 @@ public class Application extends Controller {
     
     public static Result profil() {
 		return ok(profil.render());
+    }
+    
+    public static Result addUser() {
+    	User user = Form.form(User.class).bindFromRequest().get();
+    	user.save();
+    	return redirect(routes.Application.index());
     }
 }
 
--- PVT4/app/models/User.java
+++ PVT4/app/models/User.java
@@ -1,27 +1,25 @@
 package models;
 
 import java.util.*;
+
 import javax.persistence.*;
 import javax.validation.*;
+
 import play.api.data.validation.*;
 import play.data.*;
+import play.db.ebean.Model;
 
-public class User {
+@Entity
+public class User extends Model{
 	//Tydligen skyddar Play attributen automatiskt
  // @Constraint.Required
+	
+	@Id
 	public String email;
 	public String name;
 	public String password;
 	
 	
-//	public List<ValidationError> validate() {			
-//	    List<ValidationError> errors = new ArrayList<ValidationError>();
-//	    if ((email) != null) {
-//	        errors.add(new ValidationError(email, "This e-mail is already registered."));
-//	    }
-//	    return errors.isEmpty() ? null : errors;
-//	}
-
 	
 	public User(String email, String name, String password){
 		this.email = email;
