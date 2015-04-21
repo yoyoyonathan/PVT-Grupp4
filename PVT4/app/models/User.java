1f4436b73f67d15a94ce19fdb3d7d820512a581f
Fortet9000 <adam.kvarfordt@gmail.com>
2015-04-21 11:33:53 +0200

    Revert "Merge branch 'master' of https://github.com/yoyoyonathan/PVT-Grupp4"
    Conflicts:
    	PVT4/app/views/index.scala.html

--- PVT4/app/models/User.java
+++ PVT4/app/models/User.java
@@ -8,11 +8,17 @@ import play.data.*;
 
 public class User {
 	//Tydligen skyddar Play attributen automatiskt
+<<<<<<< Updated upstream
  // @Constraint.Required
+=======
+	
+	@Id
+>>>>>>> Stashed changes
 	public String email;
 	public String name;
 	public String password;
 	
+<<<<<<< Updated upstream
 	
 //	public List<ValidationError> validate() {			
 //	    List<ValidationError> errors = new ArrayList<ValidationError>();
@@ -23,16 +29,12 @@ public class User {
 //	}
 
 	
+=======
+>>>>>>> Stashed changes
 	public User(String email, String name, String password){
 		this.email = email;
 		this.name = name;
 		this.password = password;
 	}
-	
-//	Form<User> userForm = Form.form(User.class); vette fan
-
-
-	
 	//Tydligen genererar Play getters och setters automatiskt
-
 }
