6293b856e9f9a5d773c109981016ab5646143c9b
Fortet9000 <adam.kvarfordt@gmail.com>
2015-04-21 14:06:59 +0200

    Users vy tillagd

--- PVT4/app/models/User.java
+++ PVT4/app/models/User.java
@@ -1,10 +1,8 @@
 package models;
 
 import java.util.*;
-
 import javax.persistence.*;
 import javax.validation.*;
-
 import play.api.data.validation.*;
 import play.data.*;
 import play.db.ebean.*;
