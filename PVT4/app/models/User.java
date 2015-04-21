66eb251281be798dd259879b085b13c3e7235b46
Fortet9000 <adam.kvarfordt@gmail.com>
2015-04-21 11:44:12 +0200

    Main fixar

---
+++ PVT4/app/models/User.java
@@ -0,0 +1,23 @@
+package models;
+
+import java.util.*;
+import javax.persistence.*;
+import javax.validation.*;
+import play.api.data.validation.*;
+import play.data.*;
+
+public class User {
+	//Tydligen skyddar Play attributen automatiskt
+	
+	@Id
+	public String email;
+	public String name;
+	public String password;
+	
+	public User(String email, String name, String password){
+		this.email = email;
+		this.name = name;
+		this.password = password;
+	}
+	//Tydligen genererar Play getters och setters automatiskt
+}
