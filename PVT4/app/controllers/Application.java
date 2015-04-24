934e27b87ca7f8a9e8faedf3dec05523f2266699
Fortet9000 <adam.kvarfordt@gmail.com>
2015-04-24 14:08:24 +0200

    Du måste logga in först

--- PVT4/app/controllers/Application.java
+++ PVT4/app/controllers/Application.java
@@ -107,6 +107,10 @@ public class Application extends Controller {
     }
     
     public static Result logout() {
+    	String currentUser = session("connected");
+    	if(currentUser == null) {
+            return ok(index.render("Du måste logga in först."));
+    	}
 	    session().clear();
     	return ok(index.render("You are now logged out!"));
     }
