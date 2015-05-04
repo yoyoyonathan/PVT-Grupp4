a0414a924f43f155ad9d717e53d8fbc3d2d46e4c
marialovisaxx <mami8786@student.su.se>
2015-05-04 13:17:58 +0200

    mergeee

---
+++ PVT4/main.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function() {
+
+$(document).ready(function(){
+    $("#signUp").click(function(){
+        $("#form-signup").slideDown("slow");
+    });
+  
+    $("#hide").click(function(){
+        $("p").hide();
+    });
+});
+
+});
