b09b2c31100c77057a46f65ad4db038f78957f02
Fortet9000 <adam.kvarfordt@gmail.com>
2015-05-19 09:28:32 +0200

    Revert 8f3e36d..ca9bfb9
    This rolls back to commit 8f3e36db6c0e12136abd585a328a498cf2072f40.

--- PVT4/app/controllers/Application.java
+++ PVT4/app/controllers/Application.java
@@ -82,15 +82,12 @@ public class Application extends Controller {
 					    rs.close();
 					    session("connected", userName);
 			 			return redirect(routes.Application.profilePage(userName));
-//						return redirect("/profile/" + userName);
-//					    return ok(profilePage.render(userName));
 					}
 				} 
 				
 				rs.close();
 //				return redirect("/profile/" + userName);
-//				return ok(index.render("Fel email/lösenord."));
-				return null;
+				return ok(index.render("Fel email/lösenord."));
 				
 		}catch(SQLException se){
 			//Handle errors for JDBC
@@ -531,9 +528,9 @@ public class Application extends Controller {
     
     public static Result addUser() {
 			
-//    	if (Form.form(User.class).bindFromRequest().hasErrors()){
-// 		    return badRequest(signup.render("Nu har något skrivits in fel"));
-// 		}
+    	if (Form.form(User.class).bindFromRequest().hasErrors()){
+ 		    return badRequest(signup.render("Nu har något skrivits in fel"));
+ 		}
  	    
  		User user = Form.form(User.class).bindFromRequest().get();
  		Connection conn = null;
@@ -559,10 +556,9 @@ public class Application extends Controller {
 			preparedStatement.executeUpdate();
  			// execute insert SQL statement
 
+ 			// user.save();
  			session("connected", userUserName);
  			return redirect(routes.Application.joinTeam());
-//			return redirect("/joinTeam/");
-
 
  			
  		} catch (SQLException se) {
