7ca8ef73003a972438157dd1bb1109918738e022
Fortet9000 <adam.kvarfordt@gmail.com>
2015-05-21 10:19:30 +0200

    Merge branch 'Städtant4' into backopy
    Conflicts:
    	.DS_Store
    	PVT4/app/views/profilePage.scala.html
    	PVT4/bin/views/profilePage.scala.html

--- PVT4/app/controllers/Application.java
+++ PVT4/app/controllers/Application.java
@@ -12,6 +12,7 @@ import play.libs.Json;
 public class Application extends Controller {
 	
     public static Result index() {
+    	session().clear();
         return ok(index.render(""));
     }
     
@@ -27,64 +28,6 @@ public class Application extends Controller {
 		return ok(profilePage.render(TeamDatabase.getTeam(userName), UserDatabase.getUser(userName)));
     }
     
-    public static Result login() {
-    	
-		Connection conn = null;
-		Statement stmt = null;
-    	
-    	try{
-    		
-     		User user = Form.form(User.class).bindFromRequest().get();
-			conn = DB.getConnection();
-			stmt = conn.createStatement();
-			
-			String userEmail = user.email;
-	 		String userPassword = user.password;
-		
-	 		String sql = "SELECT * FROM `user` WHERE `email` = " + "'" + userEmail + "'";
-			
-			ResultSet rs = stmt.executeQuery(sql);
-			
-			if(rs.isBeforeFirst()){
-				rs.next();
-				
-				String email = rs.getString("email");
-				String password = rs.getString("password");
-				String userName = rs.getString("userName");
-					
-					if (userEmail.equals(email) && userPassword.equals(password)){
-					    rs.close();
-					    session("connected", userName);
-			 			return redirect(routes.Application.profilePage(userName));
-					}
-				} 
-				
-				rs.close();
-//				return redirect("/profile/" + userName);
-				return ok(index.render("Fel email/lösenord."));
-				
-		}catch(SQLException se){
-			//Handle errors for JDBC
-	        return internalServerError(se.toString());
-		}catch(Exception e){
-	    	//Handle errors for Class.forName
-	        return internalServerError(e.toString());
-	 	}finally{
-			 //finally block used to close resources
-			 try{
-			    if(stmt!=null)
-			       conn.close();
-			 }catch(SQLException se){
-			 }// do nothing
-			 try{
-			    if(conn!=null)
-			       conn.close();
-			 }catch(SQLException se){
-			    return internalServerError(se.toString());
-			 }//end finally try
-	   	}//end try
-    }
-    
     public static Result logout() {
     	String currentUser = session("connected");
     	if(currentUser == null) {
@@ -92,61 +35,5 @@ public class Application extends Controller {
     	}
 	    session().clear();
     	return ok(index.render("Du är nu utloggad."));
-    }
-	    	
-    public static Result getUsers() {			//Används inte i nuläget för något
-    	
-    	ObjectNode result = Json.newObject();
-		Connection conn = null;
-		Statement stmt = null;
-    	
-    	try{
-    		
-			conn = DB.getConnection();
-			stmt = conn.createStatement();
-			
-			String sql = "SELECT * FROM user";
-			
-			ResultSet rs = stmt.executeQuery(sql);
-		
-		    while(rs.next()){
-
-				String email  = rs.getString("email");
-				String password = rs.getString("password");
-				String userName = rs.getString("userName");
-				int birthDate = rs.getInt("birthDate");
-				ObjectNode user = Json.newObject();
-				user.put("Email", email);
-				user.put("Password", password);
-				user.put("userName", userName);
-				user.put("birthDate", birthDate);
-				
-				result.put(email, password);
-				result.put(userName, birthDate);
-		   	}
-		    rs.close();
-
-			return ok(result);
-		}catch(SQLException se){
-			//Handle errors for JDBC
-	        return internalServerError(se.toString());
-		}catch(Exception e){
-	    	//Handle errors for Class.forName
-	        return internalServerError(e.toString());
-	 	}finally{
-			 //finally block used to close resources
-			 try{
-			    if(stmt!=null)
-			       conn.close();
-			 }catch(SQLException se){
-			 }// do nothing
-			 try{
-			    if(conn!=null)
-			       conn.close();
-			 }catch(SQLException se){
-			    return internalServerError(se.toString());
-			 }//end finally try
-	   	}//end try
-    	
     }
 }
