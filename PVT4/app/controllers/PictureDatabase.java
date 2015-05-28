b520b811d7b42e2d2452710ca85b1f0afebbb224
Fortet9000 <adam.kvarfordt@gmail.com>
2015-05-28 13:18:54 +0200

    PicDatabase cleaning

--- PVT4/app/controllers/PictureDatabase.java
+++ PVT4/app/controllers/PictureDatabase.java
@@ -51,111 +51,8 @@ import javax.xml.bind.DatatypeConverter;
 import javax.xml.datatype.DatatypeConstants;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-//import com.sun.medialib.mlib.Image;
-//import com.sun.xml.internal.ws.api.addressing.WSEndpointReference.Metadata;
-//import com.drew.imaging.ImageMetadataReader;
-//import com.drew.imaging.ImageProcessingException;
-//import com.drew.metadata.*;
-//import com.drew.metadata.exif.ExifIFD0Directory;
-//import com.drew.metadata.jpeg.JpegDirectory;
 
 public class PictureDatabase extends Controller{
-	
-	// Inner class containing image information
-	public static class ImageInformation {
-		public final int orientation;
-		public final int width;
-		public final int height;
-
-		public ImageInformation(int orientation, int width, int height) {
-			this.orientation = orientation;
-			this.width = width;
-			this.height = height;
-		}
-
-		public String toString() {
-			return String.format("%dx%d,%d", this.width, this.height,
-					this.orientation);
-		}
-	}
-
-//	public static ImageInformation readImageInformation(File imageFile)
-//			throws IOException, MetadataException, ImageProcessingException {
-//		Metadata metadata = ImageMetadataReader.readMetadata(imageFile);
-//
-//		Directory directory = metadata
-//				.getFirstDirectoryOfType(ExifIFD0Directory.class);
-//		JpegDirectory jpegDirectory = (JpegDirectory) metadata
-//				.getFirstDirectoryOfType(JpegDirectory.class);
-//
-//		int orientation = 1;
-//		try {
-//			orientation = directory.getInt(ExifIFD0Directory.TAG_ORIENTATION);
-//		} catch (MetadataException me) {
-//
-//		} catch (NullPointerException npe) {
-//			return null;
-//		}
-//		int width = jpegDirectory.getImageWidth();
-//		int height = jpegDirectory.getImageHeight();
-//
-//		return new ImageInformation(orientation, width, height);
-//	}
-
-	public static AffineTransform getExifTransformation(ImageInformation info) {
-
-		AffineTransform t = new AffineTransform();
-
-		switch (info.orientation) {
-		case 1:
-			break;
-		case 2: // Flip X
-			t.scale(-1.0, 1.0);
-			t.translate(-info.width, 0);
-			break;
-		case 3: // PI rotation
-			t.translate(info.width, info.height);
-			t.rotate(Math.PI);
-			break;
-		case 4: // Flip Y
-			t.scale(1.0, -1.0);
-			t.translate(0, -info.height);
-			break;
-		case 5: // - PI/2 and Flip X
-			t.rotate(-Math.PI / 2);
-			t.scale(-1.0, 1.0);
-			break;
-		case 6: // -PI/2 and -width
-			t.translate(info.height, 0);
-			t.rotate(Math.PI / 2);
-			break;
-		case 7: // PI/2 and Flip
-			t.scale(-1.0, 1.0);
-			t.translate(-info.height, 0);
-			t.translate(0, info.width);
-			t.rotate(3 * Math.PI / 2);
-			break;
-		case 8: // PI / 2
-			t.translate(0, info.width);
-			t.rotate(3 * Math.PI / 2);
-			break;
-		}
-
-		return t;
-	}
-
-	public static BufferedImage transformImage(BufferedImage originalImage,
-			AffineTransform transform) throws Exception {
-		AffineTransformOp affineTransformOp = new AffineTransformOp(transform,
-				AffineTransformOp.TYPE_BILINEAR);
-		BufferedImage destinationImage = new BufferedImage(
-				originalImage.getWidth(), originalImage.getHeight(),
-				originalImage.getType());
-		destinationImage = affineTransformOp.filter(originalImage,
-				destinationImage);
-
-		return destinationImage;
-	}
 	
 	public static BufferedImage resize(BufferedImage img, int newW, int newH) { 
 	    Image tmp = img.getScaledInstance(newW, newH, Image.SCALE_SMOOTH);
@@ -201,19 +98,6 @@ public class PictureDatabase extends Controller{
 				
 				finalImg = resize(img, 400, 400);
 
-//				if (readImageInformation(file) != null) {
-//					ImageInformation imageF = readImageInformation(file);
-//
-//					AffineTransform info = getExifTransformation(imageF);
-//
-//					finalImg = transformImage(img, info);
-//					
-//					ByteArrayOutputStream os = new ByteArrayOutputStream();
-//					ImageIO.write(finalImg, type, os);
-//					inputStream = new ByteArrayInputStream(os.toByteArray());
-
-//				} else {
-				
 //				File outputfile = new File("image." + type);
 //				ImageIO.write(finalImg, type, outputfile);
 				
@@ -256,13 +140,13 @@ public class PictureDatabase extends Controller{
 			conn = DB.getConnection();
 			stmt = conn.createStatement();
 			
-			String sql1 = "SELECT * FROM `teammember` WHERE `user` = " + "'" + currentUser + "'";
-			ResultSet rs1 = stmt.executeQuery(sql1);
-			rs1.next();
-			String team = rs1.getString("team");
-			rs1.close();
+//			String sql1 = "SELECT * FROM `teammember` WHERE `user` = " + "'" + currentUser + "'";
+//			ResultSet rs1 = stmt.executeQuery(sql1);
+//			rs1.next();
+			String team = TeamDatabase.getTeamName();
+//			rs1.close();
 			
-			String sql2 = "SELECT DISTINCT s.user FROM teammember s inner join userpic d on d.user = s.user WHERE `team` = " + "'" + team + "'";
+			String sql2 = "SELECT DISTINCT s.user FROM teammember s INNER JOIN userpic d ON d.user = s.user WHERE `team` = " + "'" + team + "'";
 			ResultSet rs2 = stmt.executeQuery(sql2);
 			ArrayList<String> members = new ArrayList<String>();
 			while (rs2.next()) {
@@ -319,102 +203,6 @@ public class PictureDatabase extends Controller{
 					conn.close();
 			} catch (SQLException se) {
 				return badRequest(se.toString());
-			} // end finally try
-		} // end try
-	}
-
-	public static Result getPicture() {
-		String currentUser = session("connected");
-//		if (currentUser == null) {
-//			return unauthorized(LoginUserPage
-//					.render("You have to login to access this page!"));
-//		} else {
-			Connection conn = null;
-			PreparedStatement preparedStatement = null;
-			List<Picture> pList = new ArrayList<Picture>();
-
-			try {
-
-				conn = DB.getConnection();
-
-				String insertIntoDatabase = "SELECT * FROM userpic ORDER BY created_date DESC";
-				preparedStatement = conn.prepareStatement(insertIntoDatabase);
-				ResultSet rs = preparedStatement.executeQuery();
-
-				while (rs.next()) {
-					Picture p = new Picture();
-//					p.creator = rs.getString("creator");
-//					p.pictureID = rs.getInt("pictureID");
-					pList.add(p);
-				}
-
-				rs.close();
-				return ok();
-
-			} catch (com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException ice) {
-				return badRequest(ice.toString());
-			} catch (NumberFormatException nfe) {
-				return badRequest(nfe.toString());
-			} catch (SQLException se) {
-				// Handle sql errors
-				return internalServerError(se.toString());
-			} catch (Exception e) {
-				// Handle errors for Class.forName
-				return internalServerError(e.toString());
-			} finally {
-
-				try {
-					if (conn != null)
-						conn.close();
-				} catch (SQLException se) {
-					return internalServerError(se.toString());
-				} // end finally try
-			} // end try
-		}
-	
-
-	public static Result render() {
-		Connection conn = null;
-		PreparedStatement preparedStatement = null;
-		String mimetype = null;
-		Blob image = null;
-		String currentUser = session("connected");
-		try {
-			conn = DB.getConnection();
-
-			String insertIntoDatabase = "SELECT * FROM userpic WHERE user=?";
-			preparedStatement = conn.prepareStatement(insertIntoDatabase);
-			preparedStatement.setString(1, currentUser);
-			ResultSet rs = preparedStatement.executeQuery();
-
-			while (rs.next()) {
-//				mimetype = rs.getString("mimetype");
-				image = rs.getBlob("image");
-			}
-
-			rs.close();
-
-			int blobLength = (int) image.length();
-			byte[] bytes = image.getBytes(1, blobLength);
-
-			return ok(bytes).as(mimetype);
-
-		} catch (com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException ice) {
-			return badRequest(ice.toString());
-		} catch (NumberFormatException nfe) {
-			return badRequest(nfe.toString());
-		} catch (SQLException se) {
-			// Handle sql errors
-			return internalServerError(se.toString());
-		} catch (Exception e) {
-			// Handle errors for Class.forName
-			return internalServerError(e.toString());
-		} finally {
-			try {
-				if (conn != null)
-					conn.close();
-			} catch (SQLException se) {
-				return internalServerError(se.toString());
 			} // end finally try
 		} // end try
 	}
